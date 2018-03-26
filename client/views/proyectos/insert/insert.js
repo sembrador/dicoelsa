1773289a3e533699a706f7944a59d9857a4e5271
Leandro Camaño <developer@castle-soft.com>
2018-03-26 18:03:24 -0500

    meteor-kitchen 201803262303230781

--- client/views/proyectos/insert/insert.js
+++ client/views/proyectos/insert/insert.js
@@ -33,8 +33,7 @@ Template.ProyectosInsertForm.onDestroyed(function() {
 });
 
 Template.ProyectosInsertForm.onRendered(function() {
-	pageSession.set("contactoCrudItems", []);
-
+	
 
 	pageSession.set("proyectosInsertFormInfoMessage", "");
 	pageSession.set("proyectosInsertFormErrorMessage", "");
@@ -108,7 +107,7 @@ Template.ProyectosInsertForm.events({
 			function(values) {
 				
 
-				values.contacto = pageSession.get("contactoCrudItems"); Meteor.call("proyectosInsert", values, function(e, r) { if(e) errorAction(e); else submitAction(r); });
+				Meteor.call("proyectosInsert", values, function(e, r) { if(e) errorAction(e); else submitAction(r); });
 			}
 		);
 
@@ -130,9 +129,9 @@ Template.ProyectosInsertForm.events({
 		e.preventDefault();
 
 		/*BACK_REDIRECT*/
-	}, 
+	}
 
-	'click .field-contacto .crud-table-row .delete-icon': function(e, t) { e.preventDefault(); var self = this; bootbox.dialog({ message: 'Delete? Are you sure?', title: 'Delete', animate: false, buttons: { success: { label: 'Yes', className: 'btn-success', callback: function() { var items = pageSession.get('contactoCrudItems'); var index = -1; _.find(items, function(item, i) { if(item._id == self._id) { index = i; return true; }; }); if(index >= 0) items.splice(index, 1); pageSession.set('contactoCrudItems', items); } }, danger: { label: 'No', className: 'btn-default' } } }); return false; }
+	
 });
 
 Template.ProyectosInsertForm.helpers({
@@ -141,129 +140,6 @@ Template.ProyectosInsertForm.helpers({
 	},
 	"errorMessage": function() {
 		return pageSession.get("proyectosInsertFormErrorMessage");
-	}, 
-		"contactoCrudItems": function() {
-		return pageSession.get("contactoCrudItems");
-	}
-});
-
-
-Template.ProyectosInsertFieldContactoInsertFormContainerFieldContactoInsertForm.onCreated(function() {
-	
-});
-
-Template.ProyectosInsertFieldContactoInsertFormContainerFieldContactoInsertForm.onDestroyed(function() {
-	
-});
-
-Template.ProyectosInsertFieldContactoInsertFormContainerFieldContactoInsertForm.onRendered(function() {
-	
-
-	pageSession.set("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage", "");
-	pageSession.set("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage", "");
-
-	$(".input-group.date").each(function() {
-		var format = $(this).find("input[type='text']").attr("data-format");
-
-		if(format) {
-			format = format.toLowerCase();
-		}
-		else {
-			format = "mm/dd/yyyy";
-		}
-
-		$(this).datepicker({
-			autoclose: true,
-			todayHighlight: true,
-			todayBtn: true,
-			forceParse: false,
-			keyboardNavigation: false,
-			format: format
-		});
-	});
-
-	$("input[type='file']").fileinput();
-	$("select[data-role='tagsinput']").tagsinput();
-	$(".bootstrap-tagsinput").addClass("form-control");
-	$("input[autofocus]").focus();
-});
-
-Template.ProyectosInsertFieldContactoInsertFormContainerFieldContactoInsertForm.events({
-	"submit": function(e, t) {
-		e.preventDefault();
-		pageSession.set("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage", "");
-		pageSession.set("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage", "");
-
-		var self = this;
-
-		function submitAction(result, msg) {
-			var proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormMode = "insert";
-			if(!t.find("#form-cancel-button")) {
-				switch(proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormMode) {
-					case "insert": {
-						$(e.target)[0].reset();
-					}; break;
-
-					case "update": {
-						var message = msg || "Saved.";
-						pageSession.set("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage", message);
-					}; break;
-				}
-			}
-
-			/*SUBMIT_REDIRECT*/
-		}
-
-		function errorAction(msg) {
-			msg = msg || "";
-			var message = msg.message || msg || "Error.";
-			pageSession.set("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage", message);
-		}
-
-		validateForm(
-			$(e.target),
-			function(fieldName, fieldValue) {
-
-			},
-			function(msg) {
-
-			},
-			function(values) {
-				
-
-				var data = pageSession.get("contactoCrudItems") || []; values._id = Random.id(); data.push(values); pageSession.set("contactoCrudItems", data); $("#field-contacto-insert-form").modal("hide"); e.currentTarget.reset();
-			}
-		);
-
-		return false;
-	},
-	"click #form-cancel-button": function(e, t) {
-		e.preventDefault();
-
-		$("#field-contacto-insert-form").modal("hide"); t.find("form").reset();
-
-		/*CANCEL_REDIRECT*/
-	},
-	"click #form-close-button": function(e, t) {
-		e.preventDefault();
-
-		/*CLOSE_REDIRECT*/
-	},
-	"click #form-back-button": function(e, t) {
-		e.preventDefault();
-
-		/*BACK_REDIRECT*/
-	}
-
-	
-});
-
-Template.ProyectosInsertFieldContactoInsertFormContainerFieldContactoInsertForm.helpers({
-	"infoMessage": function() {
-		return pageSession.get("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage");
-	},
-	"errorMessage": function() {
-		return pageSession.get("proyectosInsertFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage");
 	}
 	
 });
--- client/views/proyectos/update/update.js
+++ client/views/proyectos/update/update.js
@@ -33,8 +33,7 @@ Template.ProyectosUpdateForm.onDestroyed(function() {
 });
 
 Template.ProyectosUpdateForm.onRendered(function() {
-	pageSession.set("contactoCrudItems", this.data.proyecto.contacto || []);
-
+	
 
 	pageSession.set("proyectosUpdateFormInfoMessage", "");
 	pageSession.set("proyectosUpdateFormErrorMessage", "");
@@ -108,7 +107,7 @@ Template.ProyectosUpdateForm.events({
 			function(values) {
 				
 
-				values.contacto = pageSession.get("contactoCrudItems"); Meteor.call("proyectosUpdate", t.data.proyecto._id, values, function(e, r) { if(e) errorAction(e); else submitAction(r); });
+				Meteor.call("proyectosUpdate", t.data.proyecto._id, values, function(e, r) { if(e) errorAction(e); else submitAction(r); });
 			}
 		);
 
@@ -130,9 +129,9 @@ Template.ProyectosUpdateForm.events({
 		e.preventDefault();
 
 		/*BACK_REDIRECT*/
-	}, 
+	}
 
-	'click .field-contacto .crud-table-row .delete-icon': function(e, t) { e.preventDefault(); var self = this; bootbox.dialog({ message: 'Delete? Are you sure?', title: 'Delete', animate: false, buttons: { success: { label: 'Yes', className: 'btn-success', callback: function() { var items = pageSession.get('contactoCrudItems'); var index = -1; _.find(items, function(item, i) { if(item._id == self._id) { index = i; return true; }; }); if(index >= 0) items.splice(index, 1); pageSession.set('contactoCrudItems', items); } }, danger: { label: 'No', className: 'btn-default' } } }); return false; }
+	
 });
 
 Template.ProyectosUpdateForm.helpers({
@@ -141,129 +140,6 @@ Template.ProyectosUpdateForm.helpers({
 	},
 	"errorMessage": function() {
 		return pageSession.get("proyectosUpdateFormErrorMessage");
-	}, 
-		"contactoCrudItems": function() {
-		return pageSession.get("contactoCrudItems");
-	}
-});
-
-
-Template.ProyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertForm.onCreated(function() {
-	
-});
-
-Template.ProyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertForm.onDestroyed(function() {
-	
-});
-
-Template.ProyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertForm.onRendered(function() {
-	
-
-	pageSession.set("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage", "");
-	pageSession.set("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage", "");
-
-	$(".input-group.date").each(function() {
-		var format = $(this).find("input[type='text']").attr("data-format");
-
-		if(format) {
-			format = format.toLowerCase();
-		}
-		else {
-			format = "mm/dd/yyyy";
-		}
-
-		$(this).datepicker({
-			autoclose: true,
-			todayHighlight: true,
-			todayBtn: true,
-			forceParse: false,
-			keyboardNavigation: false,
-			format: format
-		});
-	});
-
-	$("input[type='file']").fileinput();
-	$("select[data-role='tagsinput']").tagsinput();
-	$(".bootstrap-tagsinput").addClass("form-control");
-	$("input[autofocus]").focus();
-});
-
-Template.ProyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertForm.events({
-	"submit": function(e, t) {
-		e.preventDefault();
-		pageSession.set("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage", "");
-		pageSession.set("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage", "");
-
-		var self = this;
-
-		function submitAction(result, msg) {
-			var proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormMode = "insert";
-			if(!t.find("#form-cancel-button")) {
-				switch(proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormMode) {
-					case "insert": {
-						$(e.target)[0].reset();
-					}; break;
-
-					case "update": {
-						var message = msg || "Saved.";
-						pageSession.set("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage", message);
-					}; break;
-				}
-			}
-
-			/*SUBMIT_REDIRECT*/
-		}
-
-		function errorAction(msg) {
-			msg = msg || "";
-			var message = msg.message || msg || "Error.";
-			pageSession.set("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage", message);
-		}
-
-		validateForm(
-			$(e.target),
-			function(fieldName, fieldValue) {
-
-			},
-			function(msg) {
-
-			},
-			function(values) {
-				
-
-				var data = pageSession.get("contactoCrudItems") || []; values._id = Random.id(); data.push(values); pageSession.set("contactoCrudItems", data); $("#field-contacto-insert-form").modal("hide"); e.currentTarget.reset();
-			}
-		);
-
-		return false;
-	},
-	"click #form-cancel-button": function(e, t) {
-		e.preventDefault();
-
-		$("#field-contacto-insert-form").modal("hide"); t.find("form").reset();
-
-		/*CANCEL_REDIRECT*/
-	},
-	"click #form-close-button": function(e, t) {
-		e.preventDefault();
-
-		/*CLOSE_REDIRECT*/
-	},
-	"click #form-back-button": function(e, t) {
-		e.preventDefault();
-
-		/*BACK_REDIRECT*/
-	}
-
-	
-});
-
-Template.ProyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertForm.helpers({
-	"infoMessage": function() {
-		return pageSession.get("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormInfoMessage");
-	},
-	"errorMessage": function() {
-		return pageSession.get("proyectosUpdateFieldContactoInsertFormContainerFieldContactoInsertFormErrorMessage");
 	}
 	
 });
